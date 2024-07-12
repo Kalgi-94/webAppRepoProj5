681f7a402c0c7c12127e36e897f49c1706959c13
Kalgi Malani <67944474+Kalgi-94@users.noreply.github.com>
2024-07-12 14:36:55 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,63 @@
+using Azure.Identity;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add Azure App Configuration to the container.
+var azAppConfigConnection = builder.Configuration["AppConfig"];
+if (!string.IsNullOrEmpty(azAppConfigConnection))
+{
+    // Use the connection string if it is available.
+    builder.Configuration.AddAzureAppConfiguration(options =>
+    {
+        options.Connect(azAppConfigConnection)
+        .ConfigureRefresh(refresh =>
+        {
+            // All configuration values will be refreshed if the sentinel key changes.
+            refresh.Register("TestApp:Settings:Sentinel", refreshAll: true);
+        });
+    });
+}
+else if (Uri.TryCreate(builder.Configuration["Endpoints:AppConfig"], UriKind.Absolute, out var endpoint))
+{
+    // Use Azure Active Directory authentication.
+    // The identity of this app should be assigned 'App Configuration Data Reader' or 'App Configuration Data Owner' role in App Configuration.
+    // For more information, please visit https://aka.ms/vs/azure-app-configuration/concept-enable-rbac
+    builder.Configuration.AddAzureAppConfiguration(options =>
+    {
+        options.Connect(endpoint, new VisualStudioCredential())
+        .ConfigureRefresh(refresh =>
+        {
+            // All configuration values will be refreshed if the sentinel key changes.
+            refresh.Register("TestApp:Settings:Sentinel", refreshAll: true);
+        });
+    });
+}
+builder.Services.AddAzureAppConfiguration();
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddApplicationInsightsTelemetry(new Microsoft.ApplicationInsights.AspNetCore.Extensions.ApplicationInsightsServiceOptions
+{
+    ConnectionString = builder.Configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"]
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseAzureAppConfiguration();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
